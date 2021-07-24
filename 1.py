bc656fc7e4df78a608b1e8f17cfa5a7c649bc9eb
Conrad Christoph <con.christoph@gmail.com>
2021-07-24 21:42:15 +0200

    24.07.2021

--- 1.py
+++ 1.py
@@ -1,5 +1,1 @@
-import requests
-from bs4 import BeautifulSoup
-page = requests.get("https://dataquestio.github.io/web-scraping-pages/simple.html")
-soup = BeautifulSoup(page.content, 'html.parser')
-list(soup.children)
+
