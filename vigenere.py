43d872b681e059bb8ff2679c2cf810a72367f420
Conrad Christoph <con.christoph@gmail.com>
2022-03-13 20:17:56 +0100

    13.03.2022

--- vigenere.py
+++ vigenere.py
@@ -1,7 +1,8 @@
 # variables
 key = input("Geben sie ihren Key an:")
 text = input("Geben sie ihren Klartext an:")
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+alphabet = ["_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+encoded_text = []
 
 # formatting of input
 text_list = text.split()
@@ -13,3 +14,17 @@ text = text.lower()
 key = key*len(text)
 key = key[:len(text)]
 
+for i in range(len(text)):
+    sorted_list = alphabet
+    index_text = alphabet.index(text[i])
+    index_key = alphabet.index(key[i])
+    for j in range(1, index_key):
+        swap = sorted_list.pop(1)
+        sorted_list.append(swap)
+    index_key = sorted_list.index(key[i])
+    encoded_text.append(sorted_list[index_key+index_text])
+    index_key = 0
+    index_text = 0
+
+
+print(encoded_text)
