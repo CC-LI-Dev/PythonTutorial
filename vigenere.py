0537951fe8e0ad5acbc6cb4229f91d47063c100c
Conrad <con.christoph@gmail.com>
2022-03-14 00:07:50 +0100

    14.03.2022

--- vigenere.py
+++ vigenere.py
@@ -1,30 +1,64 @@
 # variables
-key = input("Geben sie ihren Key an:")
-text = input("Geben sie ihren Klartext an:")
+key = input("Geben sie ihren Key ein:")
+text = input("Geben sie ihren Klartext ein:")
 alphabet = ["_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-encoded_text = []
+alphabet2 = ["_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+encoded_text_list = []
+decoded_text_list = []
 
 # formatting of input
 text_list = text.split()
 text = "".join(text_list)
 text = text.lower()
 
+key_list = key.split()
+key = "".join(key_list)
+key = key.lower()
+
 # redefintion of key
 
 key = key*len(text)
 key = key[:len(text)]
 
+# encoding through Vigenere
+
 for i in range(len(text)):
-    sorted_list = alphabet
-    index_text = alphabet.index(text[i])
+    
+    index_text = alphabet2.index(text[i])
     index_key = alphabet.index(key[i])
     for j in range(1, index_key):
-        swap = sorted_list.pop(1)
-        sorted_list.append(swap)
-    index_key = sorted_list.index(key[i])
-    encoded_text.append(sorted_list[index_key+index_text-1])
-    index_key = 0
-    index_text = 0
+        swap = alphabet.pop(1)
+        alphabet.append(swap)
+    encoded_text_list.append(alphabet[index_text])
 
+# output of encoded text
 
-print(encoded_text)
+encoded_text = "".join(encoded_text_list)
+print("Der kodierte Text ist:", encoded_text)
+
+# input of encoded text
+
+key_encoded = input("Geben sie ihren Key ein:")
+input_encoded_text = input("Geben sie ihren kodierten Text ein:")
+
+# redefintion of key
+
+key_encoded = key_encoded*len(text)
+key_encoded = key_encoded[:len(text)]
+
+# decoding
+
+for i in range(len(input_encoded_text)):
+    
+    index_key_encoded = alphabet.index(key_encoded[i])
+    for j in range(1, index_key_encoded):
+        swap = alphabet.pop(1)
+        alphabet.append(swap)
+    index_input_encoded = alphabet.index(input_encoded_text[i])
+    decoded_text_list.append(alphabet2[index_input_encoded])
+
+# output of decoded text
+decoded_text = "".join(decoded_text_list)
+print("Der dekodierte Text ist:", decoded_text)
+    
+    
