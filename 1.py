ff63cfc52cfaefd33e589228105e7fb80d8e6c54
Conrad Christoph <con.christoph@gmail.com>
2022-03-21 13:31:35 +0100

    21.03.2022

--- 1.py
+++ 1.py
@@ -1,10 +1,18 @@
-birthd = []
 from random import *
+birthd = []
+doubles = 0
+a = 100000
+occurences = 0
 
-for i in range(1, 10):
+for i in range(1, a):
     for j in range(0, 23):
         birthd.append(randint(0, 365))
-    print(birthd)
     for k in range(0, 365):
-        birthd
+        if birthd.count(k) > 1:
+            doubles = doubles+1
+    if doubles > 0:
+        occurences = occurences+1
     birthd.clear()
+
+probability = occurences/a
+print(probability)
