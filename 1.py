65754d8ebb1c3ce098710bc005baf7fe2b70dda7
Conrad Christoph <con.christoph@gmail.com>
2022-03-21 12:59:54 +0100

    21.03.2022

--- 1.py
+++ 1.py
@@ -1,3 +1,10 @@
-temperatur = []
-temperatur.append()
-print(temperatur)
+birthd = []
+from random import *
+
+for i in range(1, 10):
+    for j in range(0, 23):
+        birthd.append(randint(0, 365))
+    print(birthd)
+    for k in range(0, 365):
+        birthd
+    birthd.clear()
