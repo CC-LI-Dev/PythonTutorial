f499dd90631a1830024340b701a6fd39c6c90e56
Conrad Christoph <con.christoph@gmail.com>
2022-06-16 23:36:11 +0200

    16.06.2022

--- new_shapes.py
+++ new_shapes.py
@@ -1,14 +1,27 @@
 from turtle import *
 from math import *
-#speed(1)
+speed(10)
 
 
 def polygon_tri_draw(n, a):
+    num_of_turns = 1
     # degree calculations
     deg_tri = 360 / n
     deg_s = (180-deg_tri) / 2
     s = (a/2) / sin(radians(deg_tri/2)) 
     for i in range(n):
+        fd(s)
+        lt(180-deg_s)
+        fd(a)
+        lt(180-deg_s)
+        fd(s)
+        goto(0,0)
+        seth(0)
+        #reset()
+        lt(deg_tri*num_of_turns)
+        num_of_turns += 1
+
+
         #fd(a)
         #lt(180-deg_s)
         #fd(s)
@@ -23,6 +36,9 @@ def polygon_tri_draw(n, a):
         #fd(s)
         #lt(180)
         #lt(deg_tri)
-        
+    a_len = a + 20
+    n_up = n  
+
+    polygon_tri_draw(n_up,a_len)        
 
 polygon_tri_draw(5, 100)
---
+++ square_quatsch.py
@@ -0,0 +1,43 @@
+from turtle import *
+from math import *
+speed(10)
+
+def tri_fun():
+    width(2)
+    bgcolor("black")
+    color_number = 0 
+    col = ["", "red", "orange", "yellow", "green", "blue", "purple"]
+        
+        
+            
+    x = 20
+    for i in range(500):
+        if color_number == 6:
+            color_number = 0
+        color_number =  color_number +1
+        
+        print(color_number)
+        color("white")
+        r = (x/2) / cos(radians(30))
+        penup()
+        fd(r)
+        pendown()
+        lt(180-45)
+        fd(x)
+        lt(180-90)
+        fd(x)
+        lt(180-90)
+        fd(x)
+        lt(180-90)
+        fd(x)
+        penup()
+        seth(0)
+        goto(0,0)
+        lt(i*5)
+        pendown()
+        
+
+        x = x + 20
+
+
+tri_fun()
